2c41ffa9e05d0e651c71d539c6c42d0036411aa3
Tony-Ao <2312826928@qq.com>
2018-03-20 15:59:04 +0800

    Initial set rowIdx=50.

---
+++ Object Repository/Page_58__/a_-   4.rs
@@ -0,0 +1,57 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>a_-   4</name>
+   <tag></tag>
+   <elementGuidId>ae9724d6-b862-410f-a791-2351c6cb968d</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value>//a[@href = 'http://dl.58.com/shangbiaozhli/30729845309119x.shtml?psid=106916396199250525521743755&amp;entinfo=30729845309119_j&amp;slot=-1' and (text() = '政府备案-大连商标注册、专利申请、软件著作权登记   [4图]  ' or . = '政府备案-大连商标注册、专利申请、软件著作权登记   [4图]  ')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>http://dl.58.com/shangbiaozhli/30729845309119x.shtml?psid=106916396199250525521743755&amp;entinfo=30729845309119_j&amp;slot=-1</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>target</name>
+      <type>Main</type>
+      <value>_blank</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>t ac_linkurl</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>政府备案-大连商标注册、专利申请、软件著作权登记   [4图]  </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;infolist&quot;)/table[@class=&quot;small-tbimg ac_container&quot;]/tbody[1]/tr[@class=&quot;ac_item&quot;]/td[@class=&quot;t&quot;]/a[@class=&quot;t ac_linkurl&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Scripts/DL58TC/UpdateInfo/Script1521532306430.groovy
@@ -0,0 +1,34 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.checkpoint.CheckpointFactory as CheckpointFactory
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testcase.TestCaseFactory as TestCaseFactory
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testdata.TestDataFactory as TestDataFactory
+import com.kms.katalon.core.testobject.ObjectRepository as ObjectRepository
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+for(def rowIdx = 0; rowIdx < 50; rowIdx++) {
+	WebUI.navigateToUrl('http://dl.58.com/shangbiaozhli/30729845309119x.shtml?psid=142589358199250552531158959&entinfo=30729845309119_j&slot=-1&iuType=j_2&PGTID=0d302162-0009-3400-6882-1633f5e69e36&ClickID=1')
+	WebUI.navigateToUrl('http://dl.58.com/shangbiaozhli/33298956032065x.shtml?psid=122356584199257696400020744&entinfo=33298956032065_p&slot=-1&iuType=p_2&PGTID=0d302162-0009-3708-2d98-edff282325ab&ClickID=1')
+	WebUI.navigateToUrl('http://dl.58.com/shangbiaozhli/33297528580417x.shtml?psid=122356584199257696400020744&entinfo=33297528580417_p&slot=-1&iuType=p_2&PGTID=0d302162-0009-3708-2d98-edff282325ab&ClickID=3')
+	WebUI.navigateToUrl('http://dl.58.com/shangbiaozhli/33296699772204x.shtml?psid=122356584199257696400020744&entinfo=33296699772204_p&slot=-1&iuType=p_2&PGTID=0d302162-0009-3708-2d98-edff282325ab&ClickID=5')
+	WebUI.navigateToUrl('http://dl.58.com/shangbiaozhli/33288332591279x.shtml?psid=122356584199257696400020744&entinfo=33288332591279_p&slot=-1&iuType=p_2&PGTID=0d302162-0009-3708-2d98-edff282325ab&ClickID=7')
+}
+
+WebUI.closeBrowser()
+
